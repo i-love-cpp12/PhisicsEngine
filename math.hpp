9279899b9f068a5c18d432f7c9b0b09b1cd812c6
Oliwier Moskala <oliwiermoskala2@gmail.com>
2025-10-05 18:24:06 +0200

    add math libery

---
+++ math.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include<stdexcept>
+
+namespace Engine2D 
+{
+    float clamp(float val, float min, float max);
+    float min(float val1, float val2);
+    float max(float val1, float val2);
+};
